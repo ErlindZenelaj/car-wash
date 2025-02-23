94bfb4768c177fbc727d7acf1b76abd215bf20e4
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2025-02-23 22:23:22 +0100

    Added Mr Wash Clean Project

---
+++ js/translation.js
@@ -0,0 +1,242 @@
+const translations = {
+    "english": {
+        "title": "Best car wash in town",
+        "description": "Mr Wash Car Wash",
+        "navigation": {
+            "home": "Home",
+            "about_us": "About Us",
+            "gallery": "Gallery",
+            "pricing": "Pricing",
+            "sponsors": "Sponsors"
+        },
+        "social_media": {
+            "facebook": "Facebook",
+            "instagram": "Instagram",
+            "tiktok": "TikTok"
+        },
+        "language_options": {
+            "english": "English",
+            "albanian": "Albanian"
+        },
+        "about_us": {
+            "why_choose_us": "Why Choose Us",
+            "self_washing": "Take Control of Your Car’s Cleanliness with Self-Washing",
+            "description": "Experience the convenience of self-washing and keep your car spotless on your schedule. Our advanced self-service stations provide high-pressure washers, foaming brushes, and eco-friendly cleaning solutions to ensure a professional-level clean every time. Say goodbye to long waits and costly washes—enjoy the freedom of washing your car, your way.",
+            "prices": "Prices →"
+        },
+        "process": {
+            "step_1": {
+                "heading": "Touchless Washing",
+                "description": "Enjoy a completely touch-free car wash experience with high-pressure water jets and eco-friendly foam. Safe for all vehicle types and ensures a spotless finish."
+            },
+            "step_2": {
+                "heading": "High-Pressure Cleaning",
+                "description": "Our powerful high-pressure washers remove tough dirt, mud, and grime efficiently, ensuring a deep clean without damaging your car’s paint."
+            },
+            "step_3": {
+                "heading": "24/7 Availability",
+                "description": "Wash your car anytime with our automated self-service stations, open 24/7 for your convenience."
+            }
+        },
+        "gallery": {
+            "gallery_text": "Gallery",
+            "latest_img": "Latest Images"
+        },
+        "pricing_plans": {
+            "section_title": {
+                "pricing_plans": "Pricing plans",
+                "wash_packages": "Wash packages"
+            },
+            "tabs": {
+                "automatic_car_wash": "Automatic Car Wash",
+                "self_car_wash": "Self Car Wash"
+            },
+            "basic": {
+                "price": "5€",
+                "shampo": "Shampoo",
+                "brush": "Brush",
+                "wax": "Wax",
+                "Drying": "Drying",
+            },
+            "deluxe": {
+                "price": "6€",
+                "softener": "Softener",
+                "high_presure": "High pressure",
+                "shampo": "Shampoo",
+                "wax": "Wax",
+                "Drying": "Drying",
+            },
+            "ultimate": {
+                "price": "8€",
+                "softener": "Softener",
+                "high_presure": "High pressure",
+                "foam": "Foam",
+                "undercarriage": "Undercarriage wash",
+                "wax": "Wax",
+                "Drying": "Drying",
+            },
+            "ultra_ultimate": {
+                "price": "12€",
+                "softener": "Softener",
+                "high_presure": "High pressure",
+                "foam": "Foam",
+                "undercarriage": "Undercarriage wash",
+                "polish": "Polish",
+                "wax": "Wax",
+                "Drying": "Drying",
+            },
+            "self_car_wash_plans": {
+                "basic": {
+                    "price": "0.50€",
+                    "includes": ["100 Sec", "Shampoo", "Foam", "Pressure wash", "Wax"]
+                },
+                "deluxe": {
+                    "price": "1.00€",
+                    "includes": ["200 Sec", "Shampoo", "Foam", "Pressure wash", "Wax"]
+                },
+                "ultimate": {
+                    "price": "2.00€",
+                    "includes": ["400 Sec", "Shampoo", "Foam", "Pressure wash", "Wax"]
+                },
+                "inside_cleaning": {
+                    "price": "0.50€",
+                    "includes": ["300 Sec"]
+                }
+            }
+        },
+        "contact_info": {
+            "number": "Numbers",
+            "working_hours": "Working Hours",
+            "availability": " 24/7 Availability",
+            "address": "Our Address"
+        },
+        "footer": {
+            "links": "Links",
+            "contact_us": "Contact Us",
+            "our_services": "Our Services",
+            "all_rights": "© 2025 Mr Wash, Carwash | All Rights Reserved",
+            "description": "Experience top-notch self-service car washing with our state-of-the-art facilities, ensuring your vehicle stays pristine.",
+        }
+    },
+    "albanian": {
+        "title": "Lavazhi më i mirë në qytet",
+        "description": "Mr Wash Car Wash",
+        "navigation": {
+            "home": "Ballina",
+            "about_us": "Rreth Nesh",
+            "gallery": "Galeria",
+            "pricing": "Çmimet",
+            "sponsors": "Sponsorët"
+        },
+        "social_media": {
+            "facebook": "Facebook",
+            "instagram": "Instagram",
+            "tiktok": "TikTok"
+        },
+        "language_options": {
+            "english": "Anglisht",
+            "albanian": "Shqip"
+        },
+        "about_us": {
+            "why_choose_us": "Pse të na zgjidhni ne",
+            "self_washing": "Merrni kontrollin e pastërtisë së makinës tuaj me larjen vetë-shërbyese",
+            "description": "Përjetoni komoditetin e vetë-larjes dhe mbajeni makinën tuaj të pastër në orarin tuaj. Stacionet tona të avancuara të vetë-shërbimit ofrojnë rondele me presion të lartë, furça shkumëzuese dhe zgjidhje pastrimi miqësore me mjedisin për të siguruar një pastrim të nivelit profesional çdo herë. Thuaj lamtumirë pritjeve të gjata dhe larjeve të kushtueshme—shijoni lirinë e larjes së makinës tuaj, sipas mënyrës tuaj.",
+            "prices": "Çmimet →"
+        },
+        "process": {
+            "step_1": {
+                "heading": "Larje pa Prekje",
+                "description": "Shijoni një përvojë larjeje të makinave pa prekje me rryma uji me presion të lartë dhe shkumë miqësore me mjedisin. E sigurt për të gjitha llojet e automjeteve dhe siguron një përfundim të pastër."
+            },
+            "step_2": {
+                "heading": "Pastrim me Presion të Lartë",
+                "description": "Larësit tanë me presion të lartë heqin në mënyrë efikase baltën, papastërtitë dhe yndyrnat e vështira, duke siguruar një pastrim të thellë pa dëmtuar bojën e makinës suaj."
+            },
+            "step_3": {
+                "heading": "Disponueshmëri 24/7",
+                "description": "Lani makinën tuaj kur të doni me stacionet tona të automatizuara vetë-shërbimi, të hapura 24/7 për lehtësinë tuaj."
+            }
+        },
+        "gallery": {
+            "gallery_text": "Galeria",
+            "latest_img": "Fotot e Fundit"
+        },
+        "pricing_plans": {
+            "section_title": {
+                "pricing_plans": "Pricing plans",
+                "wash_packages": "Wash packages"
+            },
+            "tabs": {
+                "automatic_car_wash": "Automatic Car Wash",
+                "self_car_wash": "Self Car Wash"
+            },
+            "basic": {
+                "price": "5€",
+                "shampo": "Shampon",
+                "brush": "Brusha",
+                "wax": "Qiri",
+                "Drying": "Tharje",
+            },
+            "deluxe": {
+                "price": "6€",
+                "softener": "Zbutues",
+                "high_presure": "Presion i lartë",
+                "shampo": "Shampon",
+                "wax": "Qiri",
+                "Drying": "Tharje",
+            },
+            "ultimate": {
+                "price": "8€",
+                "softener": "Zbutues",
+                "high_presure": "Presion i lartë",
+                "foam": "Shkuma",
+                "undercarriage": "Larja nën shasi",
+                "wax": "Qiri",
+                "Drying": "Tharje",
+            },
+            "ultra_ultimate": {
+                "price": "12€",
+                "softener": "Zbutues",
+                "high_presure": "Presion i lartë",
+                "foam": "Shkuma",
+                "undercarriage": "Larja nën shasi",
+                "polish": "Polir",
+                "wax": "Qiri",
+                "Drying": "Tharje",
+            },
+            "self_car_wash_plans": {
+                "basic": {
+                    "price": "0.50€",
+                    "includes": ["100 Sec", "Shampoo", "Foam", "Pressure wash", "Wax"]
+                },
+                "deluxe": {
+                    "price": "1.00€",
+                    "includes": ["200 Sec", "Shampoo", "Foam", "Pressure wash", "Wax"]
+                },
+                "ultimate": {
+                    "price": "2.00€",
+                    "includes": ["400 Sec", "Shampoo", "Foam", "Pressure wash", "Wax"]
+                },
+                "inside_cleaning": {
+                    "price": "0.50€",
+                    "includes": ["300 Sec"]
+                }
+            }
+        },
+        "contact_info": {
+            "number": "Numrat",
+            "working_hours": "Orari i punës",
+            "availability": " Disponueshmëria 24/7",
+            "address": "Adresa jonë"
+        },
+        "footer": {
+            "links": "Linqet",
+            "contact_us": "Na Kontaktoni",
+            "our_services": "Shërbimet tona",
+            "all_rights": "© 2025 Mr Wash, Carwash | Të gjitha të drejtat e rezervuara",
+            "description": "Përjetoni larjen e makinave me vetë-shërbim të nivelit të lartë me pajisjet tona më të fundit, duke siguruar që automjeti juaj të mbetet i pacenuar.",
+        }
+    }
+};
+
+export default translations;
